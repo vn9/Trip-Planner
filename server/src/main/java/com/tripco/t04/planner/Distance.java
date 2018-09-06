50f4240c993f26798b7651f5d0b211b2e7f11265
aykalo <aykalo@rams.colostate.edu>
2018-09-06 17:23:32 -0600

    First effort with handling units

--- server/src/main/java/com/tripco/t04/planner/Distance.java
+++ server/src/main/java/com/tripco/t04/planner/Distance.java
@@ -16,16 +16,21 @@ public class Distance {
     public int distance;
 
     private int vincenty(){
-        double phi1 = Double.parseDouble(origin.latitude);
-        double phi2 = Double.parseDouble(destination.latitude);
-        double lambda1 = Double.parseDouble(origin.latitude);
-        double lambda2 = Double.parseDouble(destination.latitude);
+        double phi1 = Math.toRadians(Double.parseDouble(origin.latitude));
+        double phi2 = Math.toRadians(Double.parseDouble(destination.latitude));
+        double lambda1 = Math.toRadians(Double.parseDouble(origin.latitude));
+        double lambda2 = Math.toRadians(Double.parseDouble(destination.latitude));
         double delta = Math.abs(lambda1-lambda2);
         double top = Math.pow(Math.cos(phi2)*Math.sin(delta),2) + Math.pow(Math.cos(phi1)*Math.sin(phi2)-Math.sin(phi1)*Math.cos(phi2)*Math.cos(delta),2);
         top = Math.sqrt(top);
         double bottom = Math.sin(phi1) * Math.sin (phi2) + Math.cos(phi1) * Math.cos(phi2) * Math.cos(delta);
         distance = (int)Math.atan2(top,bottom);
-        distance = distance * 3959;
+        if(units == "miles") {
+            distance = distance * 3959;
+        }
+        else if(units == "kilometers") {
+            distance = distance * 6371;
+        }
         return distance;
     }
 }
