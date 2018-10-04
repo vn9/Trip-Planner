c1be22c92202158cd3443cb327201b8d1a364eab
Zhe Liu <liuzhe992zz@gmail.com>
2018-10-04 04:35:30 -0600

    Finishing the testDistance() function. This function gets the two distances from those two distances object and compares two distances with the answers already know to see that whether the distance class get the same correct result. If so, Distance class is worked.

--- server/src/test/java/com/tripco/t04/planner/TestDistance.java
+++ server/src/test/java/com/tripco/t04/planner/TestDistance.java
@@ -8,9 +8,12 @@ import org.junit.runners.JUnit4;
 import static org.junit.Assert.*;
 
 
+
 @RunWith(JUnit4.class)
 public class TestDistance {
-    private Distance distance;
+
+    private Distance distance1;
+    private Distance distance2;
     private Place test1Origin;
     private Place test1Destination;
     private Place test2Origin;
@@ -22,10 +25,19 @@ public class TestDistance {
         test1Destination = new Place("bldr","Boulder", "40.015", "-105.2706");
         test2Origin = new Place("bldr","Boulder", "40.015", "-105.2706");
         test2Destination = new Place("foco","Fort Collins", "40.5853", "-105.0844");
-        distance = new Distance( test1Origin, test1Destination,"user defined", "accurate miles", 3958.7613);
-        distance = new Distance( test2Origin, test2Destination,"user defined", "accurate miles", 3958.7613);
+        distance1 = new Distance( test1Origin, test1Destination,"user defined", "accurate miles", 3958.7613);
+        distance2 = new Distance( test2Origin, test2Destination,"user defined", "accurate miles", 3958.7613);
     }
 
+    @Test
+    public void testDistance() {
+        int test1Distance = 24;
+        int test2Distance = 41;
+        int result1 = distance1.vincenty();
+        int result2 = distance2.vincenty();
+        assertEquals(result1,test1Distance);
+        assertEquals(result2,test2Distance);
+    }
 
 
 }
