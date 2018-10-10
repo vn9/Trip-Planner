fc996466e2dfe15ce19302580384f6e5289781be
Zhe Liu <liuzhe992zz@gmail.com>
2018-10-10 13:09:27 -0600

    fix issue of "Missing a javadoc comment" mentioned by Code Climate. Change the type of version from String to Integer. Change the way to test Search.java.

--- server/src/main/java/com/tripco/t04/planner/Search.java
+++ server/src/main/java/com/tripco/t04/planner/Search.java
@@ -1,21 +1,21 @@
 package com.tripco.t04.planner;
 
 import com.google.gson.Gson;
-
 import java.util.ArrayList;
 
-//import database in this file to do the search and return a list of Place objects
-//which conform to the requirements.
-
+/**
+ * This class imports the database here and search for places which conform the requirements.
+ * It returns an ArrayList of places
+ */
 public class Search {
 
-    public String version;
+    public Integer version;
     public String type;
     public String match;
     public int limit;
     public ArrayList<Place> places;
 
-    public  Search(String version, String type,
+    public  Search(Integer version, String type,
                    String match, int limit, ArrayList<Place> places){
 
         this.version = version;
@@ -27,6 +27,9 @@ public class Search {
     }
 
     //This method return an ArrayList of place which contain those places conform to the requirements
+    /** Handles the response for an ArrayList of places object.
+     * Does the conversion from a Java class to a Json string.*
+     */
     public String getPlaces () {
         Gson gson = new Gson();
         return gson.toJson(places);
--- server/src/test/java/com/tripco/t04/planner/TestSearch.java
+++ server/src/test/java/com/tripco/t04/planner/TestSearch.java
@@ -7,7 +7,7 @@ import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 //Up to now, the search object just has a constructor. Thus, test the construction of search
 //object is enough. As more functionality added into Search.java, new test is needed.
@@ -22,21 +22,17 @@ public class TestSearch {
 
     @Before
     public void initialize() {
-        searchobj1 = new Search("3","search","", 0, places);
-        searchobj2 = new Search("3","search","", 0, places);
+        searchobj1 = new Search(3,"search","", 0, places);
+        searchobj2 = new Search(3,"search","", 0, places);
     }
 
     @Test
     public void testSearch() {
 
+        String places1 = searchobj1.getPlaces();
+        String places2 = searchobj2.getPlaces();
+        assertEquals(places1,places2);
 
-        if(searchobj1.version.equals(searchobj2.version) ||
-                searchobj1.type.equals(searchobj2.type) ||
-                searchobj1.match.equals(searchobj2.match) ||
-                searchobj1.limit == searchobj2.limit ||
-                searchobj1.places == searchobj2.places){
-            equal = true;
-        }
     }
 
 }
