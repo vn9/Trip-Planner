e3b17e9c8b7c4f2c6a9afae0e9dbe84b8d281b6a
vn9 <vnnguyen@rams.colostate.edu>
2018-11-08 18:05:27 -0700

    Remove unused imports and no need test

--- server/src/test/java/com/tripco/t04/planner/TestTrip.java
+++ server/src/test/java/com/tripco/t04/planner/TestTrip.java
@@ -1,24 +1,16 @@
 package com.tripco.t04.planner;
 
-import java.io.FileNotFoundException;
-import java.io.OutputStreamWriter;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.FileNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 import static org.junit.Assert.*;
 
-/*
-  This class contains tests for the Trip class.
- */
+//This class contains tests for the Trip class.
+
 @RunWith(JUnit4.class)
 public class TestTrip {
   Trip trip;
@@ -49,13 +41,12 @@ public class TestTrip {
     // assertTrue checks if a statement is true
     assertTrue(true == true);
     //System.out.println(trip.svg());
-
   }
 
   @Test
   public void testLegDistances() {
     trip.plan();
-    ArrayList<Integer> expectedDistances = new ArrayList<Integer>();
+    ArrayList<Integer> expectedDistances = new ArrayList<>();
     Collections.addAll(expectedDistances,  102, 97, 92, 86);
     // Call the equals() method of the first object on the second object.
     assertEquals(expectedDistances, trip.distances);
@@ -64,13 +55,13 @@ public class TestTrip {
   @Test
   public void testOptions(){
       trip.plan();
-      String opti = "none";
+      String opt = "none";
       String unit = "user defined";
       String unName = "miles";
       Double unRad = 3959.0;
       String map = "";
 
-      assertEquals(opti, trip.options.optimization);
+      assertEquals(opt, trip.options.optimization);
       assertEquals(unit, trip.options.units);
       assertEquals(unName, trip.options.unitName);
       assertEquals(unRad, trip.options.unitRadius);
@@ -79,54 +70,25 @@ public class TestTrip {
 
   @Test
   public void testOpt1(){
-    trip.options.optimization = "short";
-    trip.plan();
-    ArrayList<Place> testplaces = new ArrayList<>();
-    testplaces.add(denver); testplaces.add(minturn); testplaces.add(saguache); testplaces.add(peublo);
-    assertEquals(trip.places, testplaces);
+      trip.options.optimization = "short";
+      trip.plan();
+      ArrayList<Place> testPlaces = new ArrayList<>();
+      testPlaces.add(denver);
+      testPlaces.add(minturn);
+      testPlaces.add(saguache);
+      testPlaces.add(peublo);
+      assertEquals(trip.places, testPlaces);
   }
 
   @Test
   public void testOpt2(){
       trip.options.optimization = "shorter";
       trip.plan();
-      ArrayList<Place> testplaces = new ArrayList<>();
-      testplaces.add(denver); testplaces.add(minturn); testplaces.add(saguache); testplaces.add(peublo);
-      assertEquals(trip.places, testplaces);
+      ArrayList<Place> testPlaces = new ArrayList<>();
+      testPlaces.add(denver);
+      testPlaces.add(minturn);
+      testPlaces.add(saguache);
+      testPlaces.add(peublo);
+      assertEquals(trip.places, testPlaces);
   }
-
-   /*
-  * testMap uses BufferedReader to read in a local file
-  * stores each line in an arrayList then return a big long string
-  * testMap also uses PrintWriter to write each line that was read into a pre-created local file
-  * for visualizing purpose
-  * @param line - temporary string when read and write file
-  * @param localPath - a string of the path where the output file locates, change the path if needed*/
-
-  
-  
-  
-  /*@Test
-  public void testMap() {
-    trip.plan();
-    BufferedReader reader;
-    String line = "";
-    ArrayList<String> store = new ArrayList<>();
-    try{
-      reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/CObackground.svg")));
-      //String localPath = "";
-      //PrintWriter writer = new PrintWriter(localPath);
-      while((line = reader.readLine())!=null){
-        //System.out.println(line);
-        store.add(line);
-        //writer.print(line);
-      }
-      //writer.close();
-    }
-    catch (Exception e){}
-
-    String expect = store.toString();
-    //assertEquals(trip.map,"<svg width=\"1920\" height=\"960\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\"svg_4\"> <svg id=\"svg_1\" height=\"960\" width=\"1920\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_2\"> <title>Layer 1</title> <rect fill=\"rgb(119, 204, 119)\" stroke=\"black\" x=\"0\" y=\"0\" width=\"1920\" height=\"960\" id=\"svg_3\"/> </g> </svg> </g> <g id=\"svg_9\"> <svg id=\"svg_5\" height=\"480\" width=\"960\" y=\"240\" x=\"480\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_6\"> <title>Layer 2</title> <polygon points=\"0,0 960,0 960,480 0,480\" stroke-width=\"12\" stroke=\"brown\" fill=\"none\" id=\"svg_8\"/> <polyline points=\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\" fill=\"none\" stroke-width=\"4\" stroke=\"blue\" id=\"svg_7\"/> </g> </svg> </g> </g> </svg>");
-    assertEquals(trip.map,expect);
-  }*/
 }
