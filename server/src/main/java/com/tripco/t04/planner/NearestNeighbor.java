03248675e0be97bfd820d3b8f89e1747ff93470f
aykalo <aykalo@rams.colostate.edu>
2018-11-14 09:15:54 -0700

    Created: Optimize, NearestNeighbor, and TwoOpt classes. Called NearestNeighbor and TwoOpt differently than before

---
+++ server/src/main/java/com/tripco/t04/planner/NearestNeighbor.java
@@ -0,0 +1,38 @@
+package com.tripco.t04.planner;
+
+import java.util.ArrayList;
+
+public class NearestNeighbor extends Optimize{
+    NearestNeighbor(ArrayList<Place> places, int[][] latice){
+        super(places, latice);
+    }
+
+    private int neighbor ( int[] indices, int from){
+        int min = Integer.MAX_VALUE;
+        int j = -1;
+        for (int i = from; i < places.size(); i++) {
+            int distance = latice[indices[from - 1]][indices[i]];
+            if (distance < min) {
+                min = distance;
+                j = i;
+            }
+        }
+        if (j == -1) {
+            j = 0;
+        }
+        return j; //returns index of nearest neighbor
+    }
+
+    protected int optimizer(int[] candidate){
+        int Total = 0;
+        for (int i = 1; i < candidate.length; i++) {//for all start points
+            int nearestIndex = neighbor(candidate, i);
+            int dist = latice[candidate[i - 1]][candidate[nearestIndex]];
+            Total += dist;
+            swap(candidate, i, nearestIndex);
+        }
+        //System.out.println(bestTotal);
+        Total = Total + latice[candidate[0]][candidate[candidate.length - 1]];
+        return Total;
+    }
+}
---
+++ server/src/main/java/com/tripco/t04/planner/TwoOpt.java
@@ -0,0 +1,64 @@
+package com.tripco.t04.planner;
+
+import java.util.ArrayList;
+
+public class TwoOpt extends Optimize{
+    TwoOpt(ArrayList<Place> places, int[][] latice){
+        super(places, latice);
+    }
+
+    private void optReverse ( int[] candidate, int i1, int k){
+        int start = Math.min(i1, k);
+        int end = Math.max(i1, k);
+        while (start < end) { //copied from Dave's code. keep swapping until all swaps are done
+            swap(candidate, start++, end--); //actual swapping
+        }
+    }
+
+    private int swapAttempt ( int[] candidate, int index1, int index2, int[][] latice){
+        int c1 = candidate[index1 - 1]; // c1
+        int c2 = candidate[index1]; // c2
+        int c3 = candidate[index2]; // c3
+        int c4 = candidate[(index2 + 1) % candidate.length]; // c4
+        // latice[c1][c2] distance between point 1 and the point that is pointing towards it
+        // latice[c3][c4] distance between point 2 and the point next
+        // THE MOD IS FOR ONLY ONE CASE, WHERE WE ARE AT THE END AND NEED A LOOP
+        int candDist = latice[c1][c2] + latice[c3][c4];
+        // latice[c1][c3] the distance between point pointing to point1 and point 2
+        // latice[c2][c4] distance between point 1 and point that point 2 is pointing towards
+        int newDist = latice[c1][c3] + latice[c2][c4];
+        if (candDist > newDist) {
+            //if candDist is greater than newDist, there is a cross.
+            // Draw a picture if you don't understand this line. Really useful
+            optReverse(candidate, index1, index2); //reveres them
+            return candDist - newDist;
+        } else {
+            return 0;
+        }
+    }
+
+    private int iterate ( int[] candidate, int minDist, int[][] latice){
+        for (int i = 1; i < candidate.length - 1; i++) {
+            for (int j = i + 1; j < candidate.length; j++) {
+                //nested for loop in the psuedo code given from Dave.
+                // if there is a swap, check if we are allowed to swap
+                minDist -= swapAttempt(candidate, i, j, latice);
+            }
+        }
+        return minDist;
+    }
+
+    protected int optimizer(int[] candidate){
+        int lastDist = Integer.MAX_VALUE;
+        //runs 1opt. -> best = nn(0) in picture from Dave's office
+        Optimize oneOpt = new NearestNeighbor(places, latice);
+        int minDist = oneOpt.optimizer(candidate);
+        while (lastDist != minDist) {
+            //After running 1-opt for some certain start points, keep improving until we can't anymore
+            lastDist = minDist;
+            //Iterate through the candidate and returns the minimum total distance
+            minDist = iterate(candidate, minDist, latice);
+        }
+        return minDist;
+    }
+}
